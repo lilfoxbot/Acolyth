161db93e32551e8ffdbd9d523cd6221242e1baac
calebrichardson-bfi <leetlefoxbot@gmail.com>
2025-04-29 15:24:18 -0500

    testcube, transform

--- Acolyth.c
+++ Acolyth.c
@@ -35,16 +35,8 @@ int main(void)
     
     //-------------------------------------------------------------------------------------
 
-    // Generates some random columns
-    // float heights[MAX_COLUMNS] = { 0 };
-    // Vector3 positions[MAX_COLUMNS] = { 0 };
-    // Color colors[MAX_COLUMNS] = { 0 };
-
-    // for (int i = 0; i < MAX_COLUMNS; i++)
-    // {
-        // heights[i] = (float)GetRandomValue(1, 12);
-        // positions[i] = (Vector3){ (float)GetRandomValue(-15, 15), heights[i]/2.0f, (float)GetRandomValue(-15, 15) };
-        // colors[i] = (Color){ GetRandomValue(20, 255), GetRandomValue(10, 55), 30, 255 };
+    // Class testCube {
+        
     // }
 
     DisableCursor();                    // Limit cursor to relative movement inside the window
@@ -140,6 +132,7 @@ int main(void)
                 DrawSphere((Vector3){ 0.0f, 5.0f, 0.0f }, 1.0f, YELLOW); 
                 DrawSphereWires((Vector3){ 0.0f, 5.0f, 0.0f }, 1.0f, 20, 20, ORANGE);
                 
+                // draw arm
                 DrawCylinder((Vector3){camera.target.x, camera.target.y, camera.target.z}, 0.25f, 0.25f, 0.5f, 8, WHITE);
                 DrawCylinderWires((Vector3){camera.target.x, camera.target.y, camera.target.z}, 0.25f, 0.25f, 0.5f, 8, BLACK);
 
